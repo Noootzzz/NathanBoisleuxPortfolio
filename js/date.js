7d61b080d9c16381d0f30842fc4efbf102194e4b
Noot <nathan.github22@gmail.com>
2024-07-01 11:05:32 +0200

    Init
    Adding all V002 Portfolio files.

---
+++ js/date.js
@@ -0,0 +1,7 @@
+//year for the footer
+let year = new Date().getFullYear()
+let copyright = document.createElement("h4")
+let footer = document.querySelector("footer")
+
+copyright.textContent = `\u00A9 ${year} BOISLEUX Nathan. All Rights Reserved.`
+footer.appendChild(copyright)
